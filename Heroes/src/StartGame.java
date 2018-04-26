8ff68880ba8e797e3375e654d80c107910d4d8a4
dyanakie <dyanakie@yahoo.com>
2018-04-26 16:19:18 +0300

    Created Packages for Items and Creatures(test)

--- Heroes/src/StartGame.java
+++ Heroes/src/StartGame.java
@@ -1,2 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class StartGame {
+
+    public static void main(String[] args) throws IOException {
+        // write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+
+
+    }
 }
