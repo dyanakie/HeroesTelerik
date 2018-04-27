475488bf1da39e56f8e317e038752642b4b46d42
Mishev <m.n.mishev@gmail.com>
2018-04-27 15:30:30 +0300

    Updated some Hero methods and added Interfaces package

--- Heroes/src/HeroesTelerik/MainHero/Hero.java
+++ Heroes/src/HeroesTelerik/MainHero/Hero.java
@@ -15,6 +15,8 @@ public class Hero {
     private static final int INITIAL_LEVEL = 1;
     private static final int BASE_ATTACK = 5;
     private static final int BASE_DEFENCE = 5;
+    private static final int INITIAL_X_COORDINATE = 1;
+    private static final int INITIAL_Y_COORDINATE = 1;
 
 
     private Coordinate coordinate;
@@ -29,11 +31,12 @@ public class Hero {
 
     public Hero(String name) {
         this.name = name;
+        this.coordinate = new Coordinate(INITIAL_X_COORDINATE, INITIAL_Y_COORDINATE);
         this.heroClass = HeroClass.randomHeroClass();
         this.level = INITIAL_LEVEL;
         this.attack = BASE_ATTACK;
         this.defence = BASE_DEFENCE;
-        this.items = new ArrayList<>();
+        this.items = new ArrayList<Item>();
         this.army = new Army();
         this.gold = INITIAL_GOLD;
 
