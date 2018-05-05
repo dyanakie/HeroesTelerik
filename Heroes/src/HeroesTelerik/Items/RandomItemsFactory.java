fd95fcb347cf04f2335c165a08adce04fab0dfd3
George <george_m99@yahoo.com>
2018-05-05 20:53:19 +0300

    Finished  itemsFactory

--- Heroes/src/HeroesTelerik/Items/RandomItemsFactory.java
+++ Heroes/src/HeroesTelerik/Items/RandomItemsFactory.java
@@ -1,36 +1,45 @@
 package HeroesTelerik.Items;
 
 import HeroesTelerik.ItemType;
+
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
  * Created by Joro on 03/05/2018
- *  add constraints to min max values; add random weight?;
+ *  Item Price = value*2; (*3 for weapons)
+ *  Item Weight = value/10;
  *
- * NOTE: length of both Strings[] much match
+ * NOTE: length of both Strings[] much match!!
  */
 public class RandomItemsFactory {
     private final int MIN_VALUE = 1;
-    private final int MAX_VALUE = 15;
-    private final int MAX_PRICE = 50;
+    private final int MAX_VALUE = 30;
+    private final int POTION_QUANTITY = 30;
     private String[] swordNames = {"Machete's Blade", "Death Bane","Crystal Abyss","Stick of Truth","HellRaiser","Wooden Sword"};
     private String[] shieldNames = {"Sentinel","Skull Basher","Bone Shield","Death's Shield","BladeGuard","Brown Shield"};
     private Random   random;
+    private int      value;
 
     public Item getItem(ItemType type){
         switch (type){
-            case SHIELD:return new Shield(shieldNames[getIndex()], getValue(MAX_VALUE), getValue(MAX_PRICE),15);
-                //break;
-            case SWORD:return new Sword(swordNames[getIndex()], getValue(MAX_VALUE), getValue(MAX_PRICE),10);
-                //break;
-            case ATTACK_POTION:return new AttackPotion(getClass().toString(),getValue(MAX_PRICE),1,10);
-                //break;
+            case SHIELD: value = getValue(MAX_VALUE);
+                         return new Shield(shieldNames[getIndex()], value, value*2,value/2);
+
+            case SWORD: value = getValue(MAX_VALUE);
+                        return new Sword(swordNames[getIndex()], value, value*3,value/3);
+
+            case ATTACK_POTION: value = getValue(POTION_QUANTITY);
+                        return new AttackPotion(getClass().toString(),value*2,1,value);
+
+            case EXP_POTION: value = getValue(POTION_QUANTITY);
+                        return new ExperiencePotion(getClass().toString(),value*2,1,value);
+
             default:return null;
-                //break;
         }
     }
 
-    //Returns random int to be used as value for damage or defense
+    //Returns random int(value), used to calculate damage price weight and defense
     private int getValue(int value){
         random = new Random();
         return random.nextInt(value - MIN_VALUE) +1 + MIN_VALUE;
@@ -40,6 +49,15 @@ public class RandomItemsFactory {
     private int getIndex(){
         random = new Random();
         return random.nextInt(shieldNames.length);
+    }
+
+    public ArrayList<Item> generateItems(){
+        ArrayList<Item> items= new ArrayList<>();
+        items.add(getItem(ItemType.SHIELD));
+        items.add(getItem(ItemType.SWORD));
+        items.add(getItem(ItemType.EXP_POTION));
+
+        return items;
     }
 
 }
